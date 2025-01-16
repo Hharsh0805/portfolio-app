53910524e0e335da856afa7623720dd3d5a7053a
Harsh Sehgal <sehgalharsh700@gmail.com>
2025-01-17 00:24:06 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {}, // Provide an empty object to avoid errors
+    'process.env': JSON.stringify({}), // Properly mock process.env to avoid errors
   },
-})
+});
